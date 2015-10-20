b25856145283e2141b36370d24a3a9ca4fa6fc36
sandyws15 <swang16@email.wm.edu>
2015-10-19 22:16:53 -0400

    add start, pause, game over views

--- include/GameView.hpp
+++ include/GameView.hpp
@@ -4,23 +4,37 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include "Player.hpp"
+#include "GameLogic.hpp"
 
 namespace lava
 {
 	class GameView
 	{
 	public:
-		void update(float delta);
 		GameView(sf::RenderWindow* window, std::vector<Actor*>* actors, Player* player);
+        void update(sf::Clock clock);
+                
+        void setFont();
+        void setPauseMessage();
+        void setPause();
+        void setStartMessage();
+        void setStart();
+        void setGameoverMessage();
+        void setGameover();
 		
 	protected:
 		sf::RenderWindow* window;
-		void processInput();
+        void processInput(sf::Clock clock);
 		void draw();
 	
 	private:
 		std::vector<Actor*>* actors;
 		Player* player;
+        sf::Font font;
+        bool isWait = false;
+        bool isPlaying = false;
+        bool isGameover = false;
+        
 	};
 }
 
