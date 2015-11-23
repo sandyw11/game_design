7746ee0c30a6a763b1ec99d2d05b2a8d63983188
Will Theuer <watheuer@email.wm.edu>
2015-11-23 15:53:21 -0500

    Fix stuck moving platforms

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -22,10 +22,14 @@ namespace lava
 	void Platform::update(float delta)
 	{
 		// check bounds
-		if (getX() + platformWidth > 800 || getX() < 0
-			&& vx != 0)
+		if (vx != 0 && 
+			getX() + platformWidth > 800 || getX() < 0)
 		{
 			vx *= -1;
+
+			// fix stuck bug
+			if (getX() < 0) platformSprite.setPosition(sf::Vector2f(0, getY()));
+			if (getX() + platformWidth > 800) platformSprite.setPosition(sf::Vector2f(800 - platformWidth, getY()));
 		}
 
 		// move platform
